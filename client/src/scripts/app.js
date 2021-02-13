9f4ee9a7f01365922350e207759d1ea6e4380d79
iClusterDev <cluster.coredev@gmail.com>
2021-02-13 16:29:25 +0000

    test commit

--- client/src/scripts/app.js
+++ client/src/scripts/app.js
@@ -1,28 +1,174 @@
-// import { StaticActor } from './components/core/Actor';
 import { Buffer, Context } from './components/core/Context';
 import Engine from './components/core/Engine';
-// import squareSrc from '../images/square.svg';
-// import circleSrc from '../images/circle.svg';
-// import triangleSrc from '../images/triangle.svg';
+
+class Actor extends Buffer {
+  constructor(config) {
+    const { width = 50, height = 50, positionX = 0, positionY = 0 } = config;
+    super(width, height, positionX, positionY);
+
+    const { name, update, velocityX = 0, velocityY = 0 } = config;
+    this._name = name;
+    this._update = update;
+    this._velocityX = velocityX;
+    this._velocityY = velocityY;
+
+    this._context.fillStyle = 'red';
+    this._context.fillRect(
+      0,
+      0,
+      this._context.canvas.width,
+      this._context.canvas.height
+    );
+  }
+
+  update(timestep) {
+    this._update(timestep);
+  }
+}
 
 const screen = new Context(800, 600, true);
-const actor = new Buffer(50, 50, 0, 0);
-actor._context.fillStyle = 'green';
-actor._context.fillRect(0, 0, actor.buffer.width, actor.buffer.height);
+const ball = new Actor({
+  name: 'ball',
+  width: 50,
+  height: 50,
+  positionX: 10,
+  positionY: 20,
+  velocityX: 0.5,
+  velocityY: 0.5,
+  update: function (timestep) {
+    const minPositionX = 0;
+    const maxPositionX =
+      screen._context.canvas.width - this._context.canvas.width;
+    const minPositionY = 0;
+    const maxPositionY =
+      screen._context.canvas.height - this._context.canvas.height;
+    if (this._positionX > maxPositionX || this._positionX < minPositionX) {
+      this._velocityX = -this._velocityX;
+    }
+    if (this._positionY > maxPositionY || this._positionY < minPositionY) {
+      this._velocityY = -this._velocityY;
+    }
+    this._positionX += this._velocityX * timestep;
+    this._positionY += this._velocityY * timestep;
+  },
+});
 
-let dx = 2;
-const update = () => {
+screen.render(ball.buffer);
+
+const update = (timestep) => {
   screen.clear();
-
-  actor._positionX -= dx;
-
-  console.log('DEBUG ~ file: app.js ~ line 16 ~ update ~ dx', dx);
+  ball.update(timestep);
 };
 
 const render = () => {
-  screen.render(actor.buffer);
+  screen.render(ball.buffer);
 };
 
 const engine = new Engine(update, render);
 
-// engine.start();
+engine.start();
+
+// ----------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------
+
+const test = {
+  use: ['screen', 'controller'],
+  name: 'testActor',
+  props: {
+    width: 100,
+    height: 200,
+    positionX: 10,
+    positionY: 10,
+    velocityX: 0.5,
+    velocityY: 0.5,
+    solid: false,
+  },
+  computed: {},
+  methods: {
+    update: (timestep) => {},
+  },
+  sprite: null,
+  emit: [
+    { name: 'bouncex', action: () => {} },
+    { name: 'bouncex', action: () => {} },
+  ],
+};
+
+class Config {
+  constructor() {
+    this.use = [];
+    this.name = '';
+    this.props = {
+      width: null,
+      height: null,
+      positionX: null,
+      positionY: null,
+      velocityX: null,
+      velocityY: null,
+      solid: false,
+      skin: null,
+    };
+    this.computed = {}; // getters and setters
+    this.methods = {}; // prototype stuff
+    this.emit = []; // events
+
+    Object.seal(this);
+  }
+}
+
+class Configurable {
+  constructor(config = {}) {
+    this.config = new Config();
+    this.init(config);
+    Object.seal(this);
+  }
+
+  init(config) {
+    // id
+    this.name = config.name;
+
+    // properties
+    Object.assign(this, config.props);
+
+    // methods
+    Object.assign(Configurable.prototype, config.methods);
+  }
+}
+
+const configurable = new Configurable({
+  name: 'congigme',
+  solid: false,
+  skin: null,
+
+  properties: {
+    width: 100,
+    height: 200,
+    positionX: 10,
+    positionY: 10,
+    velocityX: 0.5,
+    velocityY: 0.5,
+  },
+
+  computed: {
+    area: function () {
+      return this.width * this.height;
+    },
+  },
+
+  methods: {
+    update1: function (timestep) {
+      console.log(this.width);
+    },
+    update2: function () {
+      console.log(this.height);
+    },
+    update3: function () {
+      console.log(this.positionX);
+    },
+  },
+});
+configurable.update1();
+configurable.update2();
+configurable.update3();
+// console.log('DEBUG ~ file: app.js ~ line 104 ~ configurable', configurable);
